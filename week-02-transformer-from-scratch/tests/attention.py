65cb810e5a0a7c3461360e6074f80282fd1477be
vaadewoyin <victoradewoyinva@gmail.com>
2026-04-11 01:39:09 +0100

    test: add unit test for padding mask weights

--- week-02-transformer-from-scratch/tests/attention.py
+++ week-02-transformer-from-scratch/tests/attention.py
@@ -1,3 +1,4 @@
+import torch
 from transformer.attention import MultiHeadAttention
 
 def test_multihead_attention(input_data):
@@ -10,3 +11,20 @@ def test_multihead_attention(input_data):
 
     assert outputs.shape == (batch_size, seq_len, emb_dim)
     assert weights.shape == (batch_size, num_heads, seq_len, seq_len)
+
+def test_padding_mask_weights(input_data):
+    """ Tests that attention weight at mask positions are zero. """
+    x, _, mask = input_data
+    batch_size, seq_len, emb_dim = x.shape
+    num_heads = 2
+    attn = MultiHeadAttention(emb_dim=emb_dim, num_heads=num_heads, dropout=0.0)
+    _, weights = attn(x, x, x, key_padding_mask=mask)
+
+    rows, cols = torch.where(mask)  # row(batch) & col(idx_pos) where mask is trur
+    for batch, mask_true_pos in zip(rows, cols):
+        # Check that attention weights for the padding cols are zero
+        assert torch.allclose(input=weights[batch, :, :, [mask_true_pos]],
+                              other=torch.tensor(0.0), atol=1e-8)
+
+    print("Padding mask test passed.")
+
