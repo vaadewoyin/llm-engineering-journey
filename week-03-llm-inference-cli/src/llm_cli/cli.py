262f3475b7e656fbbcc4418951f3f54bc15b83bf
vaadewoyin <victoradewoyinva@gmail.com>
2026-04-23 01:58:03 +0100

    feat: add compare command & update cli v1

--- week-03-llm-inference-cli/src/llm_cli/cli.py
+++ week-03-llm-inference-cli/src/llm_cli/cli.py
@@ -1,15 +1,17 @@
 # Imports
-from llm_cli.config import GenConfig, load_gen_config
-from llm_cli.generate import GenerationEngine
 import typer
 from rich.console import Console
+from rich.table import Table
+from llm_cli.config import GenConfig, load_gen_config
+from llm_cli.generate import GenerationEngine
+from llm_cli.compare import compare_models
 
 app = typer.Typer()
 console = Console()
 
 # Cli command to generate text from a model
 @app.command()
-def generate(config_file: str = typer.Option(None, help= "config file to use"), 
+def run(config_file: str = typer.Option(None, help= "config file to use"), 
              model_id: str = typer.Option(None, help="ID of the model to use"),
              prompt: str = typer.Option("What is the capital of France?", help = "prompt for text generation"),
              streamer: bool = typer.Option(True, help="Whether to use streaming output"),
@@ -42,4 +44,49 @@ def generate(config_file: str = typer.Option(None, help= "config file to use"),
     gen_engine.generate(prompt)
 
     
+# Cli command to compare two models
+@app.command()
+def compare(
+    model_id1: str = typer.Option(..., help="ID of the first model to compare"), 
+    model_id2: str = typer.Option(..., help="ID of the second model to compare"), 
+    prompt: str = typer.Option("What is the capital of France?", help="Prompt for generation")
+):
+    comparison_result = compare_models(model_id1, model_id2, prompt)
+    console.print(f"\nComparison of models '{model_id1}' and '{model_id2}' for prompt: '{prompt}'\n")
+   
+    # Get outputs and performance metrics for both models
+    model_id1_output = comparison_result["model_id1_output"]
+    model_id2_output = comparison_result["model_id2_output"]
+    model_id1_decoded_output = model_id1_output["tokenizer"].decode(model_id1_output["outputs"], skip_special_tokens=True)
+    model_id2_decoded_output = model_id2_output["tokenizer"].decode(model_id2_output["outputs"], skip_special_tokens=True)
+    model_id1_tokens_per_sec = model_id1_output["tokens_per_sec"]
+    model_id2_tokens_per_sec = model_id2_output["tokens_per_sec"]
+    model_id1_generation_time = model_id1_output["token_gen_time_sec"]
+    model_id2_generation_time = model_id2_output["token_gen_time_sec"]
+    model_id1_token_count = model_id1_output["outputs"].shape[-1]
+    model_id2_token_count = model_id2_output["outputs"].shape[-1]
 
+    # Print model decoded outputs
+    console.print(f"[bold]Model[/bold] '[bold]{model_id1}[/bold]' \n[bold]output[/bold]: {model_id1_decoded_output}")
+    console.print(f"\n[bold]Model[/bold] '[bold]{model_id2}[/bold]' \n[bold]output[/bold]: {model_id2_decoded_output}")
+
+    model_results = [
+        {"Model": model_id1, "Tokens Generated": model_id1_token_count, "Generation Time (s)": model_id1_generation_time, 
+         "Tokens/sec": model_id1_tokens_per_sec},
+        {"Model": model_id2, "Tokens Generated": model_id2_token_count, "Generation Time (s)": model_id2_generation_time, 
+         "Tokens/sec": model_id2_tokens_per_sec}
+    ]
+    
+    console.print('\n')
+    # Rich table for comparison
+    table = Table(title="Model Comparison")
+    table.add_column("Model", justify="left", style="cyan", no_wrap=True)
+    table.add_column("Tokens Generated", justify="right", style="magenta")
+    table.add_column("Generation Time (s)", justify="right", style="green")
+    table.add_column("Tokens/sec", justify="right", style="yellow")
+
+    for result in model_results:    
+        table.add_row(result["Model"], str(result["Tokens Generated"]), f"{result['Generation Time (s)']:.2f}", 
+                      f"{result['Tokens/sec']:.2f}")
+    
+    console.print(table)
